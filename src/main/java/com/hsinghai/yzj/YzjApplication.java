33480427446e4ffd5d1955ac135713016683bc30
xingHe-zhou <843048844@qq.com>
2022-11-12 12:26:01 +0800

    1.创建项目

---
+++ src/main/java/com/hsinghai/yzj/YzjApplication.java
@@ -0,0 +1,13 @@
+package com.hsinghai.yzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzjApplication.class, args);
+    }
+
+}
