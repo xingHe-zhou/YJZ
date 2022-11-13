c2ef77ab5e98adfbd509a5a9bb36e2ee4cce632c
刘昱全 <limaoainao@163.com>
2022-11-13 11:13:04 +0800

    项目准备

--- src/main/java/com/hsinghai/yzj/YzjApplication.java
+++ src/main/java/com/hsinghai/yzj/YzjApplication.java
@@ -1,13 +1,18 @@
 package com.hsinghai.yzj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/**
+ * <h2>易记账应用程序</h2>
+ *
+ * @author zh、lyq
+ */
+@MapperScan(basePackages = {"com.hsinghai.yzj.repository"})
+@SpringBootApplication(scanBasePackages = {"com.hsinghai.yzj"})
 public class YzjApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(YzjApplication.class, args);
+        new SpringApplication(YzjApplication.class).run(args);
     }
-
 }
